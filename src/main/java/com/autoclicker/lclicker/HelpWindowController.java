6af6cad1dbcb505fc4c6ee3c70f2b7d2cf0aa436
LeoZ213 <113804442+LeoZ213@users.noreply.github.com>
2023-03-17 22:03:28 -0400

    Added comments to helpController

--- src/main/java/com/autoclicker/lclicker/HelpWindowController.java
+++ src/main/java/com/autoclicker/lclicker/HelpWindowController.java
@@ -6,6 +6,12 @@ import java.net.URI;
 import java.net.URISyntaxException;
 public class HelpWindowController {
 
+    /**
+     * Handles the click event of the GitHub link button in the Help window.
+     * Opens the GitHub repository URL in the default web browser.
+     * @throws URISyntaxException if the syntax of the URI is incorrect.
+     * @throws IOException if an I/O error occurs.
+     */
     public void gitHyperLinkClicked() throws URISyntaxException, IOException {
         URI githubLink = new URI("https://github.com/LeoZ213/AutoClicker");
         if (Desktop.isDesktopSupported()){
@@ -16,6 +22,12 @@ public class HelpWindowController {
         }
     }
 
+    /**
+     * Handles the click event of the author link button in the Help window.
+     * Opens the author's GitHub profile URL in the default web browser.
+     * @throws URISyntaxException if the syntax of the URI is incorrect.
+     * @throws IOException if an I/O error occurs.
+     */
     public void authorHyperLinkClicked () throws URISyntaxException, IOException{
         URI githubProfileLink = new URI("https://github.com/LeoZ213/");
         if (Desktop.isDesktopSupported()){
